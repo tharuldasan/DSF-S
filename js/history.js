0c7261135d404c392a7ca671ad426e5a738a9545
Thraul Dasan <dasantharul@gmail.com>
2026-03-28 13:29:50 +0530

    Refactor history.js to use user-specific file keys

--- js/history.js
+++ js/history.js
@@ -1,26 +1,55 @@
-let files = JSON.parse(localStorage.getItem("files")||"[]");
+function getUserKey(){
+  let user = localStorage.getItem("currentUser");
+  return "files_" + user;
+}
 
 let div = document.getElementById("files");
 
 function renderHistory(){
+  let files = JSON.parse(localStorage.getItem(getUserKey())||"[]");
   div.innerHTML = "";
 
-  files.forEach(f=>{
-    let el = document.createElement("div");
-    el.style.margin = "10px";
-    el.style.padding = "10px";
-    el.style.background = "#eee";
-    el.style.cursor = "pointer";
+  files.forEach((f,index)=>{
+    let box = document.createElement("div");
+    box.style.margin="10px";
+    box.style.padding="10px";
+    box.style.background="#eee";
 
-    el.innerText = f.name;
+    box.innerHTML = `
+      <b>${f.name}</b><br><br>
+      <button onclick="openFile('${f.id}')">Open</button>
+      <button onclick="renameFile(${index})">Rename</button>
+      <button onclick="deleteFile(${index})">Delete</button>
+    `;
 
-    el.onclick = ()=>{
-      localStorage.setItem("currentFile", f.id);
-      window.location = "result.html";
-    };
-
-    div.appendChild(el);
+    div.appendChild(box);
   });
+}
+
+function openFile(id){
+  localStorage.setItem("currentFile", id);
+  window.location="result.html";
+}
+
+function renameFile(index){
+  let files = JSON.parse(localStorage.getItem(getUserKey())||"[]");
+
+  let name = prompt("New name:", files[index].name);
+  if(!name) return;
+
+  files[index].name = name;
+  localStorage.setItem(getUserKey(), JSON.stringify(files));
+  renderHistory();
+}
+
+function deleteFile(index){
+  let files = JSON.parse(localStorage.getItem(getUserKey())||"[]");
+
+  if(confirm("Delete?")){
+    files.splice(index,1);
+    localStorage.setItem(getUserKey(), JSON.stringify(files));
+    renderHistory();
+  }
 }
 
 renderHistory();
