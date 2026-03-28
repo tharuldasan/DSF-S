7e546057a2346be1f6cd361725374fd068d7a826
Thraul Dasan <dasantharul@gmail.com>
2026-03-28 13:09:08 +0530

    Refactor createNew to handle multiple files with ID
    Updated createNew function to include file ID and store multiple files in localStorage.

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,6 +2,8 @@ function createNew(){
   let name = prompt("Enter File Name:");
   if(!name) return;
 
+  let id = "file_" + Date.now();
+
   let keys = [];
   ["A","B","C","D","E"].forEach(l=>{
     for(let i=1;i<=5;i++){
@@ -11,12 +13,26 @@ function createNew(){
     }
   });
 
-  let emptyData = {};
-  keys.forEach(k => emptyData[k] = 0);
+  let emptyGiven = {};
+  let emptyUsed = {};
 
-  localStorage.setItem("givenData", JSON.stringify(emptyData));
-  localStorage.setItem("usedData", JSON.stringify(emptyData));
-  localStorage.setItem("fileName", name);
+  keys.forEach(k => {
+    emptyGiven[k] = 0;
+    emptyUsed[k] = 0;
+  });
+
+  let file = {
+    id: id,
+    name: name,
+    given: emptyGiven,
+    used: emptyUsed
+  };
+
+  let files = JSON.parse(localStorage.getItem("files")||"[]");
+  files.push(file);
+
+  localStorage.setItem("files", JSON.stringify(files));
+  localStorage.setItem("currentFile", id);
 
   window.location = "result.html";
 }
