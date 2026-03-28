c6e24bd47489ad9953969101c406ba559d20d033
Thraul Dasan <dasantharul@gmail.com>
2026-03-28 13:09:45 +0530

    Refactor history rendering to use createElement
    Refactored file rendering logic to use createElement for better structure and styling.

--- js/history.js
+++ js/history.js
@@ -1,47 +1,26 @@
 let files = JSON.parse(localStorage.getItem("files")||"[]");
 
-let draftDiv = document.getElementById("draft");
-let finishedDiv = document.getElementById("finished");
+let div = document.getElementById("files");
 
-files.forEach((f,i)=>{
-  let btn = `
-    <div>
-      ${f.name}
-      <button onclick="openFile(${i})">Open</button>
-      <button onclick="renameFile(${i})">Rename</button>
-      <button onclick="deleteFile(${i})">Delete</button>
-    </div>
-  `;
+function renderHistory(){
+  div.innerHTML = "";
 
-  if(f.used == null){
-    draftDiv.innerHTML += btn;
-  }else{
-    finishedDiv.innerHTML += btn;
-  }
-});
+  files.forEach(f=>{
+    let el = document.createElement("div");
+    el.style.margin = "10px";
+    el.style.padding = "10px";
+    el.style.background = "#eee";
+    el.style.cursor = "pointer";
 
-function openFile(i){
-  localStorage.setItem("currentFile", i);
-  window.location = "result.html";
+    el.innerText = f.name;
+
+    el.onclick = ()=>{
+      localStorage.setItem("currentFile", f.id);
+      window.location = "result.html";
+    };
+
+    div.appendChild(el);
+  });
 }
 
-function renameFile(i){
-  let name = prompt("New name:");
-  if(name){
-    files[i].name = name;
-    localStorage.setItem("files", JSON.stringify(files));
-    location.reload();
-  }
-}
-
-function deleteFile(i){
-  if(confirm("Delete?")){
-    files.splice(i,1);
-    localStorage.setItem("files", JSON.stringify(files));
-    location.reload();
-  }
-}
-
-function back(){
-  window.location = "dashboard.html";
-}
+renderHistory();
