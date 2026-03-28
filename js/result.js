bc96e9fa3872c69f94ced9548528f1e913a67300
Thraul Dasan <dasantharul@gmail.com>
2026-03-28 12:22:20 +0530

    Update result.js

--- js/result.js
+++ js/result.js
@@ -1,103 +1,154 @@
 let given = JSON.parse(localStorage.getItem("givenData")) || {};
 let used  = JSON.parse(localStorage.getItem("usedData")) || {};
 
-let historyStack = [];
+let level = "main";
+let current = "";
 
-function getTotals(prefix=""){
-  let result = {};
-
-  for(let key in given){
-    if(key.startsWith(prefix)){
-      let level;
-
-      if(prefix === ""){
-        level = key[0];
-      } else if(prefix.length === 1){
-        level = key.substring(1,2);
-      } else {
-        level = key;
-      }
-
-      if(!result[level]){
-        result[level] = {given:0, used:0};
-      }
-
-      result[level].given += given[key] || 0;
-      result[level].used  += used[key] || 0;
-    }
-  }
-
-  return result;
+function formatRs(num){
+  return "Rs. " + num.toLocaleString(undefined, {minimumFractionDigits:2});
 }
 
-function render(prefix=""){
-  let totals = getTotals(prefix);
+function render(){
   let div = document.getElementById("totals");
 
   let html = `
     <table>
       <tr>
         <th>Category</th>
-        <th>Given (LKR)</th>
-        <th>Used (LKR)</th>
+        <th>Sub</th>
+        <th>Item</th>
+        <th>Given</th>
+        <th>Used</th>
         <th>Balance</th>
       </tr>
   `;
 
-  for(let k in totals){
-    let g = totals[k].given;
-    let u = totals[k].used;
-    let b = g - u;
+  if(level === "main"){
+    ["A","B","C","D","E"].forEach(letter=>{
+      let g=0,u=0;
 
-    let next;
+      for(let k in given){
+        if(k.startsWith(letter)){
+          g += given[k];
+          u += used[k];
+        }
+      }
 
-    if(prefix === ""){
-      next = k;
-    } else if(prefix.length === 1){
-      next = prefix + k;
-    } else {
-      next = k;
+      html += `
+        <tr class="clickable" onclick="goLevel('${letter}')">
+          <td>${letter}</td>
+          <td></td>
+          <td></td>
+          <td>${formatRs(g)}</td>
+          <td>${formatRs(u)}</td>
+          <td>${formatRs(g-u)}</td>
+        </tr>
+      `;
+    });
+  }
+
+  else if(level === "A"){
+    for(let i=1;i<=5;i++){
+      let key = current + i;
+      let g=0,u=0;
+
+      for(let k in given){
+        if(k.startsWith(key)){
+          g += given[k];
+          u += used[k];
+        }
+      }
+
+      html += `
+        <tr class="clickable" onclick="goLevel('${key}')">
+          <td>${current}</td>
+          <td>${key}</td>
+          <td></td>
+          <td>${formatRs(g)}</td>
+          <td>${formatRs(u)}</td>
+          <td>${formatRs(g-u)}</td>
+        </tr>
+      `;
     }
+  }
 
-    html += `
-      <tr class="clickable" onclick="drill('${next}')">
-        <td>${next}</td>
-        <td>${g}</td>
-        <td>${u}</td>
-        <td>${b}</td>
-      </tr>
-    `;
+  else if(level === "A1"){
+    for(let j=1;j<=5;j++){
+      let key = current + "." + j;
+      let g = given[key] || 0;
+      let u = used[key] || 0;
+
+      html += `
+        <tr>
+          <td>${current[0]}</td>
+          <td>${current}</td>
+          <td>${key}</td>
+
+          <td class="clickable" onclick="edit('${key}','given')">
+            ${formatRs(g)}
+          </td>
+
+          <td class="clickable" onclick="edit('${key}','used')">
+            ${formatRs(u)}
+          </td>
+
+          <td>${formatRs(g-u)}</td>
+        </tr>
+      `;
+    }
   }
 
   html += "</table>";
   div.innerHTML = html;
 }
 
-function drill(next){
-  historyStack.push(next);
-  render(next);
+function goLevel(val){
+  if(val.length === 1){
+    level = "A";
+  }else{
+    level = "A1";
+  }
+  current = val;
+  render();
 }
 
 function goBack(){
-  historyStack.pop();
-  let prev = historyStack[historyStack.length - 1] || "";
-  render(prev);
+  if(level === "A1"){
+    level = "A";
+    current = current[0];
+  }else if(level === "A"){
+    level = "main";
+    current = "";
+  }
+  render();
+}
+
+function edit(key,type){
+  let value = prompt("Enter amount for " + key);
+  if(value === null) return;
+
+  value = Number(value) || 0;
+
+  if(type === "given"){
+    given[key] = value;
+    localStorage.setItem("givenData", JSON.stringify(given));
+  }else{
+    used[key] = value;
+    localStorage.setItem("usedData", JSON.stringify(used));
+  }
+
+  render();
 }
 
 render();
-
-function enterUsed(){
-  localStorage.setItem("mode","used");
-  window.location = "create.html";
-}
 
 function save(){
   let files = JSON.parse(localStorage.getItem("files")||"[]");
 
   files.push({
-    name:"File "+Date.now(),
+    name: localStorage.getItem("fileName") || "File",
     given: given,
-    used: Object.keys(used).length ? used : null
+    used: used
   });
 
   localStorage.setItem("files", JSON.stringify(files));
@@ -106,29 +157,4 @@ function save(){
 
 function goDashboard(){
   window.location = "dashboard.html";
-}
-
-/* ✅ EXPORT TO EXCEL */
-function exportExcel(){
-  let data = [];
-
-  for(let key in given){
-    let g = given[key] || 0;
-    let u = used[key] || 0;
-    let b = g - u;
-
-    data.push({
-      Category: key,
-      Given_LKR: g,
-      Used_LKR: u,
-      Balance: b
-    });
-  }
-
-  let ws = XLSX.utils.json_to_sheet(data);
-  let wb = XLSX.utils.book_new();
-
-  XLSX.utils.book_append_sheet(wb, ws, "Accounting");
-
-  XLSX.writeFile(wb, "Accounting_Report.xlsx");
 }
