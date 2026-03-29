bb79316a4b47094d656a438323c93679a946132a
Thraul Dasan <dasantharul@gmail.com>
2026-03-29 09:38:56 +0530

    Update item-history.js

--- js/item-history.js
+++ js/item-history.js
@@ -8,28 +8,39 @@ let id = localStorage.getItem("currentFile");
 
 let file = files.find(f=>f.id===id);
 
-let history = (file.history && file.history[key]) || [];
+let history = (file.history && file.history[key]) || {
+  given: [],
+  used: []
+};
 
-let html = `
-<table>
-<tr>
-<th>Date</th>
-<th>Status</th>
-<th>Type</th>
-<th>Amount</th>
-</tr>
-`;
+function buildTable(title, data){
+  let html = `<h3>${title}</h3>`;
 
-history.forEach(h=>{
-  html+=`
+  html += `
+  <table>
   <tr>
-    <td>${h.date}</td>
-    <td>${h.status}</td>
-    <td>${h.type}</td>
-    <td>Rs. ${h.amount}</td>
-  </tr>`;
-});
+    <th>Date</th>
+    <th>Status</th>
+    <th>Amount</th>
+  </tr>
+  `;
 
-html+="</table>";
+  data.forEach(h=>{
+    html+=`
+    <tr>
+      <td>${h.date}</td>
+      <td>${h.status}</td>
+      <td>Rs. ${h.amount}</td>
+    </tr>`;
+  });
 
-document.getElementById("historyTable").innerHTML = html;
+  html += "</table><br>";
+  return html;
+}
+
+let output = "";
+
+output += buildTable("Allo / Distribution", history.given);
+output += buildTable("Expenditure", history.used);
+
+document.getElementById("historyTable").innerHTML = output;
