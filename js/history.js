7e9a5ce3172b902579eb36a79c1d34a8c849fbf9
Thraul Dasan <dasantharul@gmail.com>
2026-03-29 08:36:05 +0530

    Update history.js

--- js/history.js
+++ js/history.js
@@ -1,9 +1,9 @@
 function getUserKey(){
-  let user = localStorage.getItem("currentUser");
-  return "files_" + user;
+  return "files_" + localStorage.getItem("currentUser");
 }
 
 let div = document.getElementById("files");
+let renameIndex = null;
 
 function renderHistory(){
   let files = JSON.parse(localStorage.getItem(getUserKey())||"[]");
@@ -12,13 +12,13 @@ function renderHistory(){
   files.forEach((f,index)=>{
     let box = document.createElement("div");
     box.style.margin="10px";
-    box.style.padding="10px";
+    box.style.padding="15px";
     box.style.background="#eee";
 
     box.innerHTML = `
       <b>${f.name}</b><br><br>
       <button onclick="openFile('${f.id}')">Open</button>
-      <button onclick="renameFile(${index})">Rename</button>
+      <button onclick="openRename(${index})">Rename</button>
       <button onclick="deleteFile(${index})">Delete</button>
     `;
 
@@ -31,25 +31,36 @@ function openFile(id){
   window.location="result.html";
 }
 
-function renameFile(index){
+function openRename(index){
+  renameIndex = index;
+  document.getElementById("renameModal").style.display="flex";
+}
+
+function closeRename(){
+  document.getElementById("renameModal").style.display="none";
+}
+
+function confirmRename(){
   let files = JSON.parse(localStorage.getItem(getUserKey())||"[]");
 
-  let name = prompt("New name:", files[index].name);
-  if(!name) return;
+  let newName = document.getElementById("renameInput").value.trim();
+  if(!newName) return;
 
-  files[index].name = name;
+  files[renameIndex].name = newName;
+
   localStorage.setItem(getUserKey(), JSON.stringify(files));
+
+  closeRename();
   renderHistory();
 }
 
 function deleteFile(index){
   let files = JSON.parse(localStorage.getItem(getUserKey())||"[]");
 
-  if(confirm("Delete?")){
-    files.splice(index,1);
-    localStorage.setItem(getUserKey(), JSON.stringify(files));
-    renderHistory();
-  }
+  files.splice(index,1);
+
+  localStorage.setItem(getUserKey(), JSON.stringify(files));
+  renderHistory();
 }
 
 renderHistory();
