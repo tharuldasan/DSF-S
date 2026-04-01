d374fb73d8a5d99b8b5908b2bfb651ef0f7c33d5
Thraul Dasan <dasantharul@gmail.com>
2026-04-01 15:55:48 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "YOUR_PROJECT_URL";
-const SUPABASE_KEY = "YOUR_ANON_KEY";
+const SUPABASE_URL = "https://voenpsxzpirhuysviyul.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvZW5wc3h6cGlyaHV5c3ZpeXVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwMzEzNzMsImV4cCI6MjA5MDYwNzM3M30.MpA_0Gykvv9-ZQA1jkBCk1zcp-t-9jkwHacaHG2-MYw";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
