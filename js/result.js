6c3da63d47d78032ccd05e1d94a4cc7422944819
Thraul Dasan <dasantharul@gmail.com>
2026-04-01 19:12:58 +0530

    Update result.js

--- js/result.js
+++ js/result.js
@@ -1,3 +1,9 @@
+const SUPABASE_URL = "https://voenpsxzpirhuysviyul.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvZW5wc3h6cGlyaHV5c3ZpeXVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwMzEzNzMsImV4cCI6MjA5MDYwNzM3M30.MpA_0Gykvv9-ZQA1jkBCk1zcp-t-9jkwHacaHG2-MYw";
+
+const { createClient } = supabase;
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
+
 let currentKey = "";
 let currentType = "";
 let lastSearch = null;
